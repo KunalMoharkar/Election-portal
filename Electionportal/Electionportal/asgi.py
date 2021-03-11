a8dfc1c26c943fb38530ec5734a8d9c7f92ca097
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2021-03-11 11:42:33 +0530

    initial commit

---
+++ Electionportal/Electionportal/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Electionportal project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Electionportal.settings')
+
+application = get_asgi_application()
