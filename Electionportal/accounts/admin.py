ccf6d2c6e3406d97f728ac7d34e691d2309e87e0
Suruchi Shrey <shreysuruchi22@gmail.com>
2021-03-24 18:20:52 +0530

    Created Models in accounts app

--- Electionportal/accounts/admin.py
+++ Electionportal/accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Department, Year, Role, Student, Voter, Profile, Candidate 
 
 # Register your models here.
+admin.site.register(Department)
+admin.site.register(Year)
+admin.site.register(Role)
+admin.site.register(Student)
+admin.site.register(Voter)
+admin.site.register(Profile)
+admin.site.register(Candidate)
--- Electionportal/accounts/models.py
+++ Electionportal/accounts/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Department(models.Model):
+    code = models.CharField(max_length=3)
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.code} ({self.name})"
+
+class Year(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.id} ({self.name})"
+
+class Role(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.id} ({self.name})"
+
+class Student(models.Model):
+    user = models.OneToOneField(User, 
+          on_delete = models.CASCADE)
+    department = models.ForeignKey(Department,on_delete = models.CASCADE, related_name="Dept_Students")
+    year = models.ForeignKey(Year,on_delete = models.CASCADE, related_name="Students")
+    roles = models.ManyToManyField(Role,related_name="Members")
+
+class Voter(models.Model):
+    student = models.OneToOneField(Student,on_delete = models.CASCADE)
+
+class Profile(models.Model):
+    image = models.ImageField(upload_to='images/')
+    linkedin_url = models.URLField(max_length = 200)
+
+class Candidate(models.Model):
+    student = models.OneToOneField(Student,on_delete = models.CASCADE)
+    profile = models.OneToOneField(Profile,on_delete = models.CASCADE)
