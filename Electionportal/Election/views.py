649c7bb1ede8608789bfc72b690b4363cfd9071b
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2021-03-27 23:29:59 +0530

    urls mapping

--- Electionportal/Election/views.py
+++ Electionportal/Election/views.py
@@ -1,3 +1,32 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+#this view serves the available eletions for a student
+#both for application as well as voting 
+def get_elections(request):
+
+    #filter on user dept
+
+    #filter on user year
+    return HttpResponse("<h1>Get election view</h1>")
+
+#this view increments the vote of a candidate for a 
+#particular election
+def vote_election(request,election_id,candidate_id):
+
+    #check first that the candidate has not already voted
+
+
+
+    #insert voter into votescasted
+
+    return HttpResponse("<h1>vote election view</h1>")
+
+#returns votewise sorted list of candidates
+#for a particaluar election
+def election_results(request,election_id):
+
+    return HttpResponse("<h1>election results view</h1>")
