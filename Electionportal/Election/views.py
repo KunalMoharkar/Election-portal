46734ab09740b7428246efdc8572a84ab79ba507
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2021-03-28 16:56:05 +0530

    voting view

--- Electionportal/Election/views.py
+++ Electionportal/Election/views.py
@@ -1,9 +1,44 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
-from . models import Votesreceived
+from . models import Election, Votesreceived, Votescasted
+from accounts.models import Voter
 
 # Create your views here.
 
+#helper function to determine if a election is open for a student
+def check_validity(voter_id, election_id):
+
+    election = Election.objects.get(id = election_id)
+    voter = Voter.objects.get(id = voter_id)
+
+    voter_year = voter.student.year.name
+    voter_dept = voter.student.department.code
+
+    print(voter_year)
+    print(voter_dept)
+
+    allowed_depts = election.allowed_depts.all()
+    allowed_years = election.allowed_years.all()
+
+    dept_list = []
+    year_list = []
+
+    for x in allowed_depts:
+        dept_list.append(x.code)
+    
+    for x in allowed_years:
+        year_list.append(x.name)
+    
+    print(dept_list)
+    print(year_list)
+
+    #cleck for year and dept also
+    if (voter_year in year_list) and (voter_dept in dept_list):
+
+        print('allowed')
+        return True 
+
+    return False
 
 #this view serves the available eletions for a student
 #both for application as well as voting 
@@ -19,6 +54,30 @@ def get_elections(request):
 def vote_election(request,election_id,candidate_id):
 
     #check first that the candidate has not already voted
+    if not Votescasted.objects.filter(voter_id = 2, election_id = election_id):
+        
+        #now check that voter can vote in that election
+        if check_validity(2,election_id) == True:
+
+            #add vote
+            votesrec = Votesreceived.objects.get(election_id = election_id, candidate_id = candidate_id)
+            votesrec.votes = votesrec.votes + 1
+            votesrec.save()
+
+            #register that voter has voted
+            votescasted = Votescasted(voter_id = 2, election_id = election_id)
+            votescasted.save()
+
+            return render(request, 'success.html')
+
+        else:
+
+            return render(request,'error.html')
+        
+    else:
+        return render(request, 'error.html')
+
+
 
 
 
