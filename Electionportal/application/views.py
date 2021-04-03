8402329002641b132abcf9ced54cc642a61fcbd4
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2021-04-03 22:28:31 +0530

    camapign msg form

--- Electionportal/application/views.py
+++ Electionportal/application/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from . models import Application, Status
+from Election.models import Election
 from accounts.models import Candidate, Voter
 from Election.views import check_validity
 
@@ -10,27 +11,36 @@ from Election.views import check_validity
 @login_required
 def apply(request,election_id):
 
-    #check that candidate exists
-    user_id = request.user.id
-    if Candidate.objects.filter(student__user__id = user_id):
-        
-        #now check he is allowed in this election
-        voter = Voter.objects.get(student__user__id = user_id)
+    if request.method == "GET":
 
-        if check_validity(voter.id, election_id) == True:
+        election = Election.objects.get(id = election_id)  
+        context = {'election':election}   
+        return render(request,"application/apply.html",context)
 
-            candidate = Candidate.objects.get(student__user__id = user_id)
-            status = Status.objects.get(name = 'in review')
-            application = Application(election_id = election_id, candidate = candidate, status = status, campaign_msg="abc")
-            application.save()
-            context = {'message':"Successfully Applied"}
-            return render(request, 'success.html',context)
-        
-        else:
+    else:
+        #check that candidate exists
+        user_id = request.user.id
+        if Candidate.objects.filter(student__user__id = user_id):
+
+            #now check he is allowed in this election
+            voter = Voter.objects.get(student__user__id = user_id)
+
+            if check_validity(voter.id, election_id) == True:
+                
+                msg = request.POST.get('msg')
+
+                candidate = Candidate.objects.get(student__user__id = user_id)
+                status = Status.objects.get(name = 'in review')
+                application = Application(election_id = election_id, candidate = candidate, status = status, campaign_msg=msg)
+                application.save()
+                context = {'message':"Successfully Applied"}
+                return render(request, 'success.html',context)
+
+            else:
+                context = {'message':"Forbidden", 'code':403}
+                return render(request, 'error.html',context)
+
+        else :
+
             context = {'message':"Forbidden", 'code':403}
             return render(request, 'error.html',context)
-
-    else :
-
-        context = {'message':"Forbidden", 'code':403}
-        return render(request, 'error.html',context)
