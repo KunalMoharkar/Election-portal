e6c87a12bcade1f9474a8e00358c590080bc1f2a
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2021-04-04 01:06:51 +0530

    basic test setup

--- Electionportal/application/tests.py
+++ Electionportal/application/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.http import response
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Application, Status
+from accounts.models import Student, Candidate, Role, Year, Department, Profile
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class ApplicationTestCase(TestCase):
+
+    def setUp(self):
+
+        user = User.objects.create_user('testuser', 'testuser@test.com', 'testpassword')
+        year = Year.objects.create(name="third")
+        role = Role.objects.create(name='candidate')
+        department = Department.objects.create(code='CSE', name='Computer Science') 
+        student = Student.objects.create(user = user, department = department,year= year, contact='9325598530')
+        student.roles.add(role)
+        profile = Profile.objects.create(linkedin_url="https://www.google.com")
+        candidate = Candidate.objects.create(student = student, profile = profile)
+
+
+    
+    def test_insertion(self):
+        self.assertEqual(Student.objects.count(), 1)
+  
+
+
+
