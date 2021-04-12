85f052bec948d719b19b2d682f373c2055aeb5ac
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2021-04-12 18:33:18 +0530

    basic testing of applications app

--- Electionportal/application/tests.py
+++ Electionportal/application/tests.py
@@ -30,14 +30,10 @@ class ApplicationTestCase(TestCase):
 
 
         election = Election.objects.create(title="test election 1",application_win=application_win, voting_win= voting_win)
-
         election.allowed_depts.add(department)
         election.allowed_years.add(year)
-
-
         status_inreview = Status.objects.create(name='in review')
         status_approved = Status.objects.create(name='approved') 
-
 
     def test_apply_view(self):
 
@@ -70,11 +66,17 @@ class ApplicationTestCase(TestCase):
         #redirection successful
         self.assertEqual(response.context['message'],"Successfully Applied")
 
-    #def test_list_blogs(self):
-    #    response = self.client.get('/blogs/')
-    #    self.assertEqual(response.status_code,200)
-    #    self.assertEqual(response.context['blogs'].count(),1)
-  
+    def test_my_aplications_view(self):
+        
+        
+        
+        self.client.login(username='testuser', password='testpassword')
+
+        response = self.client.get('/application/myapplications/')
+        self.assertEqual(response.status_code, 200)
+
+        #no application is applied by the voter
+        self.assertEqual(response.context['applications'].count(),0)
 
 
 
--- Electionportal/application/urls.py
+++ Electionportal/application/urls.py
@@ -5,3 +5,4 @@ urlpatterns = [
     path('apply/<int:election_id>',views.apply,name="apply_election"),
     path('myapplications/',views.my_applications,name="my_applications"),
 ]
+
